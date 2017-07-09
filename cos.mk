f0eaaf8113bd7a2d99ca5fee29a47cb01235306e
jackcicci <jackcicci@hotmail.it>
2017-07-09 20:38:15 +0200

    Update and rename lineage.mk to cos.mk

--- cos.mk
+++ cos.mk
@@ -1,9 +1,11 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/common.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := lineage_titan
+PRODUCT_NAME := cos_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+COS_RELEASE=true
